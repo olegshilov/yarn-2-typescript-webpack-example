d8e3c9579107f6fc565761a00cc0051c6f92412c
Oleg Shilov <me@olegshilov.com>
2021-03-17 20:11:24 +0300

    chore: update dependencies

--- packages/workspace-a/src/index.tsx
+++ packages/workspace-a/src/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
+
+import './styles.ts';
+
 import { Root } from './components/Root';
-import './styles';
 
 function createApp(): void {
   const container = document.getElementById('root');
